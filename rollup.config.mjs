5664de7796e51c55ca3f0527747106ffcc8a67f3
Craig Yu <craig.yu93@gmail.com>
2025-06-17 10:47:59 -0700

    fix: json hanlde

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,5 @@
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
@@ -49,6 +50,7 @@ const handlebarsPlugin = () => ({
 
 const getPlugins = () => {
     const plugins = [
+        json(),
         nodeResolve(),
         commonjs({
             sourceMap: false,
